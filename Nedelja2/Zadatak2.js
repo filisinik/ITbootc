83b5a94c92259356a8f35156be5e895f6d373901
NikolaP <p>
2020-02-05 22:47:15 +0100

    Proba11

--- Nedelja2/Zadatak2.js
+++ Nedelja2/Zadatak2.js
@@ -4,8 +4,13 @@
 //      console.log('Uspesno ste kupili prozivod');
 //   }else{console.log('Nemate dovoljno novca');}
 
- let cena  = 10;
- let kupac = 100;
+  let cena  = 101;
+  let kupac = 100;
+ let promet = function(cena, kupac){
+     console.log(kupac - cena);
+     
+ }
+ promet(cena,kupac);
  if( kupac - cena >=0){
      console.log('Uspesno ste kupili prozivod')
  }else{
