dbbba606b9797f40b6dd524e2e4e1121beecdd30
NikolaP <p>
2020-02-05 20:18:33 +0100

    Proba1

---
+++ Nedelja2/Zadatak2.js
@@ -0,0 +1,13 @@
+//  let cena = 101;
+//  let kupac = 100;
+//  if(cena < kupac && cena <=kupac || cena ===kupac ){
+//      console.log('Uspesno ste kupili prozivod');
+//   }else{console.log('Nemate dovoljno novca');}
+
+ let cena  = 10;
+ let kupac = 100;
+ if( kupac - cena >=0){
+     console.log('Uspesno ste kupili prozivod')
+ }else{
+     console.log('Nemate dovoljno novca')
+ }
